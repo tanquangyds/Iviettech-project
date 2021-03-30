8c76d248743d22a30837ac4693530598a8d88ce4
Tan Quang <tanquang@Tans-MacBook-Pro.local>
2021-03-30 11:29:32 +0700

    update development

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import {Provider} from "react-redux";
-import store from "./store";
+import store from "./redux/store";
+// import './assets/scss/index.scss';
 import './App.css';
-import Nav from "./components/Nav"
-import Home from "./components/Home"
-import Cart from "./components/Cart"
-import Details from "./components/Details";
+import Nav from "./pages/Nav/Nav"
+import Home from "./pages/Home/Home"
+import Cart from "./pages/Cart/Cart"
+import Details from "./pages/Details/Details";
 
 function App() {
   return (
     <Router>
      <Provider store={store}>
-     <Nav />
-     <Route path="/" exact component={Home} />
-     <Route path="/cart" exact component={Cart} />
-     <Route path="/details/:id" exact component={Details} />
-     </Provider>
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/cart" exact component={Cart} />
+        <Route path="/details/:id" exact component={Details} />
+      </Provider>
     </Router>
   );
 }
